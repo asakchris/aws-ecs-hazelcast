0e90e726a1b6b7f56526b6ea19fb1469203af264
Kamaraj Christopher <k.appadurai@spglobal.com>
2019-10-20 20:55:05 -0400

    Refactor

--- hazelcast-client/src/main/java/com/example/client/service/CacheServiceImpl.java
+++ hazelcast-client/src/main/java/com/example/client/service/CacheServiceImpl.java
@@ -2,13 +2,22 @@ package com.example.client.service;
 
 import com.example.client.cache.Cache;
 import com.example.client.vo.Token;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class CacheServiceImpl implements CacheService {
+    private static Logger log = LoggerFactory.getLogger(CacheServiceImpl.class);
+
     private final Cache cache;
+
+    @Autowired
+    private CacheManager cacheManager;
 
     public CacheServiceImpl(Cache cache) {
         this.cache = cache;
@@ -16,7 +25,13 @@ public class CacheServiceImpl implements CacheService {
 
     @Override
     public Token cacheToken(Token token) {
-        return cache.cache(token.getUsername(), token);
+        Token cache = this.cache.cache(token.getUsername(), token);
+        Token token1 = cacheManager.getCache("token-cache").get(token.getUsername(), Token.class);
+        log.info("token from cache manger for user name: {}, token: {}", token.getUsername(), token1);
+        if (cache == null) {
+            throw new ResponseStatusException(HttpStatus.FAILED_DEPENDENCY, "Not able to store in cache");
+        }
+        return cache;
     }
 
     @Override
